dbbd6a0facab19ecced32665e561565d3da09f48
Takao Sumitomo <cattaka@mail.cattaka.net>
2019-07-11 17:59:26 +0900

    Impl PhotoDialog

--- app/src/main/java/net/cattaka/android/learnscopedstorage/dialog/PhotoDialog.kt
+++ app/src/main/java/net/cattaka/android/learnscopedstorage/dialog/PhotoDialog.kt
@@ -1,9 +1,12 @@
 package net.cattaka.android.learnscopedstorage.dialog
 
+import android.app.Dialog
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.core.os.bundleOf
 import net.cattaka.android.learnscopedstorage.databinding.DialogPhotoBinding
@@ -13,7 +16,25 @@ class PhotoDialog : AppCompatDialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DialogPhotoBinding.inflate(inflater, container, false)
+        binding.root.setOnClickListener { v ->
+            dismiss()
+        }
         return binding.root
+    }
+
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState).apply {
+            window?.requestFeature(Window.FEATURE_NO_TITLE)
+            window?.setBackgroundDrawable(ColorDrawable(android.graphics.Color.TRANSPARENT))
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        dialog?.window?.let { w ->
+            w.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        }
     }
 
     override fun onStart() {
