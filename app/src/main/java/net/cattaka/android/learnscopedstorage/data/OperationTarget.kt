e1ebb7d2b1e4c376c4e11b438ae13f5e2f7cc4c4
Takao Sumitomo <cattaka@mail.cattaka.net>
2019-07-11 16:33:20 +0900

    Buttons

--- app/src/main/java/net/cattaka/android/learnscopedstorage/data/OperationTarget.kt
+++ app/src/main/java/net/cattaka/android/learnscopedstorage/data/OperationTarget.kt
@@ -21,20 +21,46 @@ enum class OperationTarget {
     }
 
     fun delete(activity: Activity, info: OperationInfo) {
-        File(info.pathValue).takeIf { it.exists() }?.let { it.delete() }
-    }
-
-    fun read(activity: Activity, info: OperationInfo) {
-        Toast.makeText(activity, "Not implemented yet.", Toast.LENGTH_SHORT).show()
-    }
-
-    @Throws(IOException::class)
-    fun write(activity: Activity, info: OperationInfo) {
-        FileOutputStream(File(info.pathValue)).use { fout ->
-            FileInputStream(File(info.assetFileValue)).use { fin ->
-                fin.copyTo(fout)
+        wrap(activity) {
+            File(info.pathValue).let {
+                if (it.exists()) {
+                    it.delete()
+                } else {
+                    Toast.makeText(activity, "File is not exist : ${it.path}", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
 
+    fun read(activity: Activity, info: OperationInfo) {
+        wrap(activity) {
+            Toast.makeText(activity, "Not implemented yet.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @Throws(IOException::class)
+    fun write(activity: Activity, info: OperationInfo) {
+        wrap(activity) {
+            FileOutputStream(File(info.pathValue)).use { fout ->
+                FileInputStream(File(info.assetFileValue)).use { fin ->
+                    fin.copyTo(fout)
+                }
+            }
+        }
+    }
+
+    private fun wrap(activity: Activity, run: () -> Unit) {
+        try {
+            run()
+        } catch (e: Exception) {
+            val sb = StringBuilder("Error:")
+            var e2: Throwable? = e
+            while (e2 != null) {
+                sb.append("\n")
+                sb.append(e2.message)
+                e2 = e2.cause
+            }
+            Toast.makeText(activity, sb.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
 }
