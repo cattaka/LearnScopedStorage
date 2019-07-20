cdeb940eafef28e933f84fd3c65df5c5e63c3b75
Takao Sumitomo <cattaka@mail.cattaka.net>
2019-07-20 22:14:25 +0900

    Add some paths

--- app/src/main/java/net/cattaka/android/learnscopedstorage/PathListActivity.kt
+++ app/src/main/java/net/cattaka/android/learnscopedstorage/PathListActivity.kt
@@ -2,6 +2,7 @@ package net.cattaka.android.learnscopedstorage
 
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import net.cattaka.android.learnscopedstorage.databinding.ActivityPathListBinding
@@ -20,8 +21,19 @@ class PathListActivity : AppCompatActivity() {
 
     private fun createList() {
         val pairs = mutableListOf<Pair<String, Any?>>(
+            Pair("Environment.getDataDirectory()", Environment.getDataDirectory()),
+            Pair(
+                "Environment.getDownloadCacheDirectory()",
+                Environment.getDownloadCacheDirectory()
+            ),
+            Pair(
+                "Environment.getExternalStorageDirectory()",
+                Environment.getExternalStorageDirectory()
+            ),
+            Pair("Environment.getRootDirectory()", Environment.getRootDirectory()),
             Pair("cacheDir", cacheDir),
             Pair("codeCacheDir", codeCacheDir),
+            Pair("databasePath(\"foo.db\")", getDatabasePath("foo.db")),
             Pair("externalCacheDir", externalCacheDir),
             Pair("externalCacheDirs", externalCacheDirs),
             Pair("externalMediaDirs", externalMediaDirs),
